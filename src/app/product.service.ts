ca9ef55ecd655a3c242f48b3843a3848b4946bb2
Giuliano Panzironi <giuliano.panzironi@accenture.com>
2020-05-14 16:40:29 +0200

    Fix casting to products array

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,13 +15,13 @@ export class ProductService {
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(
-      response => <Album> response.json()
+      response => <Album>response.json()
     );
   }
 
   getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl).map(
-      response => <Product[]> response.json()
+      response => <Product[]>response.json()
     );
   }
 
